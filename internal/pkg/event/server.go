abf0f0320f15f822f685292d6bf37ed5b562a964
alsritter <alsritter@outlook.com>
2023-06-24 19:04:48 +0800

    add: Event Server

--- internal/pkg/event/server.go
+++ internal/pkg/event/server.go
@@ -6,10 +6,12 @@ import (
 	bus "github.com/alsritter/nsq-event-bus"
 )
 
+var ServerInstance *Server
+
 type Message struct {
 	MessageID string
 	Timestamp int64
-	Body      any
+	Body      string
 }
 
 type Option func(*Server)
@@ -26,6 +28,12 @@ func WithMaxAttempts(attempts int) Option {
 	}
 }
 
+func WithMaxInFlight(inFlight int) Option {
+	return func(s *Server) {
+		s.maxInFlight = inFlight
+	}
+}
+
 type HandlerFunc func(message *Message) error
 
 type Subscriber interface {
@@ -35,13 +43,20 @@ type Subscriber interface {
 }
 
 type Server struct {
-	handlerConcurrency int // 消费者并发数
-	maxAttempts        int // 最大尝试次数
+	lookup             []string // nsqlookupd 地址
+	address            string   // nsqd 地址
+	maxInFlight        int      // 最大并发数
+	handlerConcurrency int      // 消费者并发数
+	maxAttempts        int      // 最大尝试次数
 	subscriberList     []Subscriber
+
+	emitter *bus.Emitter // 发布者
 }
 
-func NewEvnetServer(options ...Option) *Server {
+func NewEventServer(address string, lookup []string, options ...Option) *Server {
 	srv := &Server{
+		lookup:             lookup,
+		address:            address,
 		handlerConcurrency: 4,
 		maxAttempts:        5,
 	}
@@ -50,12 +65,23 @@ func NewEvnetServer(options ...Option) *Server {
 		option(srv)
 	}
 
+	var err error
+	srv.emitter, err = bus.NewEmitter(bus.EmitterConfig{
+		Address:     srv.address,
+		MaxInFlight: srv.maxInFlight,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	ServerInstance = srv
 	return srv
 }
 
 func (s *Server) Start(ctx context.Context) error {
 	for _, subscriber := range s.subscriberList {
 		if err := bus.On(bus.ListenerConfig{
+			Lookup:             s.lookup,
 			Topic:              subscriber.GetEventName(),
 			Channel:            subscriber.GetName(),
 			HandlerFunc:        s.WrapHandler(subscriber.HandleMessage),
@@ -78,6 +104,38 @@ func (s *Server) Register(subscribers ...Subscriber) {
 }
 
 func (s *Server) WrapHandler(handler HandlerFunc) bus.HandlerFunc {
+	return bus.HandlerFunc(func(message *bus.Message) (any, error) {
+		if message.Attempts > uint16(s.maxAttempts) {
+			message.Finish()
+			return nil, nil
+		}
 
-	return nil
+		m := Message{
+			MessageID: string(message.ID[:]),
+			Timestamp: message.Timestamp,
+			Body:      string(message.Payload),
+		}
+
+		if err := handler(&m); err != nil {
+			message.Requeue(-1)
+			return nil, err
+		}
+
+		message.Finish()
+		return nil, nil
+	})
+}
+
+/**
+ * 事件发布，入参是指针
+ */
+func (s *Server) Emit(eventName string, payload any) error {
+	return s.emitter.Emit(eventName, payload)
+}
+
+/**
+ * 事件发布，入参是指针
+ */
+func (s *Server) EmitAsync(eventName string, payload any) error {
+	return s.emitter.EmitAsync(eventName, payload)
 }
